3d94cabac613f39b30f7e238feee06150443f862
Christian Bernasconi <christian.bernasconi96@gmail.com>
2021-03-13 15:34:52 +0100

    changed results format and fixed parameters (fix #7)

--- dbpediaNEL/DBPediaEntityExtractor.py
+++ dbpediaNEL/DBPediaEntityExtractor.py
@@ -2,16 +2,17 @@ import spacy_dbpedia_spotlight
 import spacy
 
 class DBPediaEntityExtractor():
-    __init__(self):
+    def __init__(self):
         # load model and keep only ner
-        nlp = spacy.load('en_core_web_lg', 
+        print('Loading \'en_core_web_lg\' model...')
+        self.nlp = spacy.load('en_core_web_lg', 
                         disable=['tok2vec', 'tagger', 'parser', 'attribute_ruler', 'lemmatizer'])
         # add dbpedia-spotlight stage
-        nlp.add_pipe('dbpedia_spotlight', config={'overwrite_ents': True})
+        self.nlp.add_pipe('dbpedia_spotlight', config={'overwrite_ents': True})
     
-    def extract(text):
+    def extract(self, text):
         # execute NER and NEL
-        doc = nlp(text)
+        doc = self.nlp(text)
         nel_ents = doc.ents
         
         # filter entities
@@ -23,9 +24,17 @@ class DBPediaEntityExtractor():
                 for ner_ent in ner_ents:
                     # keep only entities extracted with both spacy's NER and dbpedia-spotlight
                     if ner_ent.text == nel_ent.text:
-                        filtered_ents.append((nel_ent.kb_id_, nel_ent.text))
+                        ent = {
+                            'id': nel_ent.kb_id_,
+                            'text': nel_ent.text
+                        }
+                        filtered_ents.append()
             except:
                 # no NER ents, keep all the dbpedia-spotlight ones
-                filtered_ents.append((nel_ent.kb_id_, nel_ent.text))
+                ent = {
+                            'id': nel_ent.kb_id_,
+                            'text': nel_ent.text
+                        }
+                filtered_ents.append(ent)
         
         return filtered_ents
